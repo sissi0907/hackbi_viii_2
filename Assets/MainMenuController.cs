9b0d0e5984f83736e144857bb318d61dcd737a2c
sissi0907 <sissiwang99@gmail.com>
2025-01-19 07:30:33 -0500

    first commit

---
+++ Assets/MainMenuController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public CanvasGroup OptionPanel;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        gameObject.SetActive(false);
+    }
+
+    public void Option()
+    {
+        OptionPanel.alpha = 1;
+        OptionPanel.blocksRaycasts = true;
+    }
+
+    public void Back()
+    {
+        OptionPanel.alpha = 0;
+        OptionPanel.blocksRaycasts = false;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
